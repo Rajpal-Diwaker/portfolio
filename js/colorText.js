d502ffc761a7d39cb5479f39a21c1fcdd1036d0c
Ankit verma <ankitvermamat@gmail.com>
2019-06-10 05:13:45 +0530

    finished cards for mob version

--- js/colorText.js
+++ js/colorText.js
@@ -47,11 +47,11 @@
 
 
 
-// var colors = ["blue", "red", "brown", "cyan", "pink"];
-// var texts = document.getElementsByClassName('tt');
-// for (let i = 0; i < texts.length; i++) {
-//     var t = texts[i];
-//     let c = Math.floor((Math.random() * (4)) % 5);
-//     t.style.color = colors[c];
-//     console.log(c + " " + colors[c])
-// }
+var colors = ["#5a5e35","#5e3535","#ff0000","#0095b7","#4006dd","#ea09db","#a04e60","#383133","#288c34","#b2ff70"];
+var texts = document.getElementsByClassName('tt');
+for (let i = 0; i < texts.length; i++) {
+    var t = texts[i];
+    let c = Math.floor((Math.random() * (10)+1) % 5);
+    t.style.color = colors[c];
+    console.log(c + " " + colors[c])
+}
